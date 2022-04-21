d671559c3a3f12678306bd8ccd6bd5ffe9822ade
jcavazos83 <50219215+jcavazos83@users.noreply.github.com>
2022-04-21 14:57:36 -0500

    Stopped trying to genereate divs

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
+/*
 document.addEventListener('DOMContentLoaded', () => {
 
 
-    
+
 })
+*/
+
+var BLOCKS_PER_CHART = 20;
+
+function generateChart(chartContainer) {
+  var container = document.createElement("div");
+  var blockDiv;  // used in the for loop
+
+  container.className = "container2";
+  document.getElementById(chartContainer.replace("#", "")).appendChild(container);
+
+  for(var i = 0; i < BLOCKS_PER_CHART; i++) {
+    blockDiv = document.createElement("div");
+    blockDiv.className = "block";
+    container.append(blockDiv);
+  }
+}
+
+generateChart("#body");
